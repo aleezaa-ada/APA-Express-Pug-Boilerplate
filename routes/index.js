6c2fed7da30d44c884e57e2fbb14f736ee8d78e1
aleezaa-ada <aleeza.ahmed@ada.ac.uk>
2025-02-18 10:51:36 +0000

    added dynamic route, and list route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 // Home route
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express Pug Boilerplate' });
+  res.render('index', { title: 'Express Pug Boilerplate' ,foods: []});
 });
+
+router.get('/dynamic/:test', (req, res) => {
+  res.render('test',  { title: req.params.test, foods: [] });
+})
+
+router.get('/list', (req,res) => {
+  const items = [
+    {name: "Pizza"}, 
+    {name: "Pasta"}, 
+    {name: "Noodles"}, 
+    {name: "KFC"}, 
+    {name: "Sandwich"}
+  ]
+  res.render('index', {title: "This is a List", foods: items })
+})
+
 
 module.exports = router;
